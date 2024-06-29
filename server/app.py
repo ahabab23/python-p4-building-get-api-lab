d6511d51b0eb7ca33628edbeb50ffbb01ac352c6
Uwes Ahabab <uwesahabab@gmail.com>
2024-06-29 11:45:50 +0300

    Get Api Lab

--- server/app.py
+++ server/app.py
@@ -20,19 +20,34 @@ def index():
 
 @app.route('/bakeries')
 def bakeries():
-    return ''
+
+    bakeries = [bakery.to_dict() for bakery in Bakery.query.all()]
+    response = make_response(jsonify(bakeries), 200)
+    return response
 
 @app.route('/bakeries/<int:id>')
 def bakery_by_id(id):
-    return ''
+    bakery = Bakery.query.filter(Bakery.id == id).first()
+    if bakery:
+        response = make_response(jsonify(bakery.to_dict()), 200)
+    else:
+        response = make_response(jsonify({"error": "Bakery not found"}), 404)
+    return response
 
 @app.route('/baked_goods/by_price')
 def baked_goods_by_price():
-    return ''
+    baked_goods=[bg.to_dict() for bg in BakedGood.query.order_by(BakedGood.price.desc()).all()]
+    response = make_response(jsonify(baked_goods), 200)
+    return response
 
 @app.route('/baked_goods/most_expensive')
 def most_expensive_baked_good():
-    return ''
+    bg = BakedGood.query.order_by(BakedGood.price.desc()).first()
+    if bg:
+        response = make_response(jsonify(bg.to_dict()), 200)
+    else:
+        response = make_response(jsonify({"error": "No baked goods found"}), 404)
+    return response
 
 if __name__ == '__main__':
-    app.run(port=5555, debug=True)
+    app.run(port=5556, debug=True)
